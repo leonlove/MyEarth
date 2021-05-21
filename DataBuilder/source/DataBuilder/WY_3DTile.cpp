74888fbfe432c5cb2b6f24338f00045736a1329f
leonlove <2415630511@qq.com>
2021-05-21 08:38:57 +0800

    创建3DTile类，准备进行导入导出工作

---
+++ DataBuilder/source/DataBuilder/WY_3DTile.cpp
@@ -0,0 +1,26 @@
+#include "WY_3DTile.h"
+
+
+namespace BANBAN
+{
+	WY_3DTile::WY_3DTile()
+	{
+	}
+
+
+	WY_3DTile::~WY_3DTile()
+	{
+	}
+
+	bool WY_3DTile::Import(const std::string& inPath)
+	{
+
+	}
+
+	bool WY_3DTile::Export(const std::string& outPath)
+	{
+
+	}
+
+}
+
