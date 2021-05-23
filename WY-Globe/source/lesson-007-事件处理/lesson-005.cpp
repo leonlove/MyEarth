10d6f9a64cf5764418ba88f6286f9a0ad85077f8
leonlove <2415630511@qq.com>
2021-05-23 09:19:48 +0800

    OpenGL帧循环 事件处理 glew引入

---
+++ WY-Globe/source/lesson-007-事件处理/lesson-005.cpp
@@ -0,0 +1,20 @@
+// lesson-002.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "lesson-005.h"
+#include "CELLWinApp.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+	CELL::CELLWinApp app;
+	app.createWindow(800, 600, hInstance);
+	app.main(0, 0);
+	return 0;
+}
---
+++ WY-Globe/source/lesson-008-glew引入/CELLWinApp.h
@@ -0,0 +1,149 @@
+#pragma once
+#include "CELLApp.h"
+#include <windows.h>
+#include <tchar.h>
+#include "CELLPlatform.hpp"
+#include "CELLGLContext.hpp"
+
+namespace CELL
+{
+	class CELLWinApp : public CELLApp 
+	{
+	public:
+		HWND _hWnd;
+		CELLGLContext _context;
+	public:
+		CELLWinApp::CELLWinApp()
+		{
+			_hWnd = 0;
+		}
+
+	public:
+		bool createWindow(int width, int heigth, INSTANCE hWnd)
+		{
+			//1. 注册窗口
+			WNDCLASSEXW wcex;
+
+			wcex.cbSize = sizeof(WNDCLASSEX);
+
+			wcex.style = CS_HREDRAW | CS_VREDRAW;
+			wcex.lpfnWndProc = WndProc;
+			wcex.cbClsExtra = 0;
+			wcex.cbWndExtra = 0;
+			wcex.hInstance = hWnd;
+			wcex.hIcon = 0;
+			wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+			wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+			wcex.lpszMenuName = 0;
+			wcex.lpszClassName = _T("CELL.BigMap");
+			wcex.hIconSm = 0;
+
+			RegisterClassExW(&wcex);
+
+			//2. 创建窗口
+			_hWnd = CreateWindow(_T("CELL.BigMap"), _T("BigMap"), WS_OVERLAPPEDWINDOW,
+				CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hWnd, this);
+
+			if (!_hWnd)
+			{
+				return false;
+			}
+
+			//3. 显示窗口
+			ShowWindow(_hWnd, SW_SHOW);
+			UpdateWindow(_hWnd);
+
+			HDISPLAY hDC = GetDC(_hWnd);
+			if (!_context.init(_hWnd, hDC))
+			{
+				DestroyWindow(_hWnd);
+				return false;
+			}
+			
+			return true;
+		}
+
+		int main(int argc, char** argv)
+		{
+			MSG msg = { 0 };
+			// 主消息循环: 
+			while (msg.message != WM_QUIT)
+			{
+				if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+				{
+					TranslateMessage(&msg);
+					DispatchMessage(&msg);
+				}
+				render();
+			}
+			_context.shutdown();
+			return 0;
+		}
+
+		void render()
+		{
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glClearColor(1, 0, 0, 1);
+
+			_context.swapBuffer();
+		}
+
+		LRESULT eventProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+		{
+			switch (message)
+			{
+			case WM_LBUTTONDOWN:
+				break;
+			case WM_LBUTTONUP:
+				break;
+			case WM_MOUSEMOVE:
+				break;
+			case WM_MOUSEWHEEL:
+				break;
+			case WM_PAINT:
+			{
+				PAINTSTRUCT ps;
+				HDC hdc = BeginPaint(hWnd, &ps);
+				// TODO: 在此处添加使用 hdc 的任何绘图代码...
+				EndPaint(hWnd, &ps);
+			}
+			break;
+			case WM_DESTROY:
+				PostQuitMessage(0);
+				break;
+			default:
+				return DefWindowProc(hWnd, message, wParam, lParam);
+			}
+			return S_OK;
+		}
+
+	protected:
+		static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+		{
+			if (WM_CREATE == message)
+			{
+				CREATESTRUCT* pSTRUCT = (CREATESTRUCT*)lParam;
+				CELLWinApp*		pApp = (CELLWinApp*)pSTRUCT->lpCreateParams;
+				#ifndef GWL_USERDATA
+				#define GWL_USERDATA (-21)
+				#endif
+				SetWindowLongPtr(hWnd, GWL_USERDATA, (LONG_PTR)pApp);
+				//我觉得这句是没有太多必要的，除非WM_CREATE消息的同时，还有其他的消息
+				//return pApp->eventProc(hWnd, message, wParam, lParam);
+			}
+			else
+			{
+				CELLWinApp* pApp = (CELLWinApp*)GetWindowLongPtr(hWnd, GWL_USERDATA);
+				if (pApp)
+				{
+					return pApp->eventProc(hWnd, message, wParam, lParam);
+				}
+				else
+				{
+					return DefWindowProc(hWnd, message, wParam, lParam);
+				}
+			}
+			return S_OK;
+		}
+	};
+}
