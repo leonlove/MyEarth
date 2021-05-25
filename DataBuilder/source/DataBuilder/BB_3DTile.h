28363f9596782512e05cf937569f545f053572bb
leonlove <2415630511@qq.com>
2021-05-25 15:31:09 +0800

    填充3dtile数据结构 增加Json相关操作

---
+++ DataBuilder/source/DataBuilder/BB_3DTile.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+namespace BANBAN
+{
+	/*
+	 * 数据版本信息
+	 */
+	struct Asset {
+		std::string _version;
+		std::string _tilesetVersion;
+	};
+
+	/*
+	 * 数据集的外包框信息
+	 */
+	struct Porperties {
+		double	_height[2];
+		double	_longitude[2];
+		double	_latitude[2];
+	};
+
+	struct BoundingVolume {
+		double	_region[6];
+	};
+
+	struct Content {
+		std::string		_uri;
+		BoundingVolume  _boundingVolume;
+	};
+
+	struct Node {
+		BoundingVolume	_boundingVolume;
+		double			_geometricError;
+		std::string		_refine;
+		Content			_Content;
+		Node*			_child;
+	};
+
+	class BB_3DTile
+	{
+	public:
+		BB_3DTile();
+		~BB_3DTile();
+
+	public:
+		bool Import(const std::string& inPath);
+
+		/*
+		 * 将3DTile数据导出到指定路径
+		 */
+		bool Export(const std::string& outPath);
+
+	private:
+		Asset		_asset;
+		Porperties	_properties;
+		double		_geometricError;
+		Node		_root;
+	};
+}
+
+
