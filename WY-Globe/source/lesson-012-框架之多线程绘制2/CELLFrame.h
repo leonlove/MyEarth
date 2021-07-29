245f96b0104233fb6f7b0258810107c341ab489f
leonlove <2415630511@qq.com>
2021-07-29 23:57:44 +0800

    多线程绘制，在主线程中初始化opengl环境，然后在主线程中解除绑定，然后在线程中再次绑定。 通过事件来进行线程之间的通信

---
+++ WY-Globe/source/lesson-012-框架之多线程绘制2/CELLFrame.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "CELLContext.h"
+
+namespace CELL {
+
+	class CELLFrame {
+
+	public:
+		CELLFrame(CELLContext& context):_context(context)
+		{
+		}
+
+		virtual ~CELLFrame(){}
+
+	public:
+		virtual void update() = 0;
+
+		virtual void onFrameStart() = 0;
+
+		virtual void onFrameEnd() = 0;
+
+		virtual bool	onLButtonDown(int x, int y) = 0;
+
+		virtual bool	onLButtonUp(int x, int y) = 0;
+
+		virtual bool	onMouseMove(int x, int y) = 0;
+
+		virtual bool	onMouseWheel(int x, int y) = 0;
+
+	public:
+		CELLContext  _context;
+	};
+}
